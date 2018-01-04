384f877abc0e22150a69290bbd5047ba80fea780
Bernhard Miehl <bernhardmiehl@ADMINs-MacBook-Pro.local>
2018-01-04 18:40:52 +0100

    init branch

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task("webpack-dev-server", function(callback) {
 });
 
 gulp.task('css', function(){
-    return gulp.src(['src/css/*.css', 'bootstrap/dist/css/bootstrap.min.css'])
+    return gulp.src(['src/css/*.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css'])
     .pipe(minifyCSS())
     .pipe(gulp.dest('dist/css'))
 });
--- src/js/functions/shop.js
+++ src/js/functions/shop.js
@@ -4,7 +4,7 @@ exports.createShop = function () {
             //$('#shop-items').html()
             $.getJSON("data/hardware.json", function( data ) {  
                 $.each( data.hardware, function( key, val ) {
-                    var htmlBody = "<div><button type=\"button\" class=\"btn btn-secondary \" id='buy-"+val.id+"'>Buy "+val.name+"</button><span>+ "+val.miningEarnings+" BTC</span></div>";
+                    var htmlBody = "<li class='list-group-item'><button type=\"button\" class=\"btn btn-secondary \" id='buy-"+val.id+"'>Buy "+val.name+"</button><span>+ "+val.miningEarnings+" BTC</span></li>";
                     $('#shop-items').html($('#shop-items').html() + htmlBody);
                 });
             });
