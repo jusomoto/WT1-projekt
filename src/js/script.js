16950ccc252a8c3338f9d18a5858b160a0964928
Florian Walzer <flo.walzer@gmail.com>
2018-01-25 15:09:04 +0100

    fixed copy&paste bug

--- src/js/script.js
+++ src/js/script.js
@@ -193,7 +193,7 @@ function initGameEndPopUp(isTheGameWon){
   $('#gameEndUserName').html(currentUser);
   $('#gameEndDollar').html(currentDollar);
   $('#gameEndBTC').html(currentBTC);
-  $('#gameEndTime').html(currentBTC);
+  $('#gameEndTime').html(currentGameTime + "sec");
 }
 
 window.onbeforeunload = confirmExit;
